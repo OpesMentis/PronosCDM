2528d1e4ddf6115c92b84d2d083c234ac6b56b14
OpesMentis <planchot.antoine@gmail.com>
2018-03-24 15:23:53 +0100

    Création d'une page pour la prédiction du déroulé de l'ensemble du tournoi

---
+++ general.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+    header('Location: index.php');
+    exit();
+}
+?>
+
+<html>
+<head>
+<title>Parier sur un match | Pronostics coupe du monde 2018</title>
+    <link href='https://fonts.googleapis.com/css?family=Mina'
+    rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans'
+    rel='stylesheet'>
+    <link href='style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div align="left">
+        <font style="font-family: 'Mina'; font-size: 20px;"><a href="index.php"><b>Pronostics CDM 2018</b></a></font>
+    </div>
+    <div align="right">
+        <font style="font-family: 'Open Sans'; font-size: 20px;"><a href="logout.php">Déconnexion</a></font>
+    </div><br/><br/>
+    <div align="center">
+        <?php
+        include('connect.php');
+        ?>
+        <font style="font-family: 'Open Sans'; font-size: 30px;"><b>Voilà ce qui va se passer...</b><br/><br/></font>
+    </div>

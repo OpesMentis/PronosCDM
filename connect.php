4b1af557ddefaa85c57724615532f39aaf5e4dd4
OpesMentis <planchot.antoine@gmail.com>
2018-03-29 22:22:51 +0200

    Revert "Inclusion d'un fichier de configuration modèle pour la base de données"
    This reverts commit 8f192f73041cce06063264c32c7150b8e8de6a77.

--- connect.php
+++ connect.php
@@ -1,11 +1,5 @@
 <?php
-
-if (!file_exists(__DIR__ . '/config.ini.dist')) {
-    echo 'You must copy config.ini.dist to config.ini and fill it with your information.';
-}
-
-$ini = parse_ini_file(__DIR__ . '/config.ini');
-
+$ini = parse_ini_file('config.ini');
 try {
     $bdd = new PDO('mysql:host=' . $ini['db_address'] . ';dbname=' . $ini['db_name'] . ';charset=utf8', $ini['db_user'], $ini['db_password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {

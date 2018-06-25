09622bb4042b133cec270ec523cd1cb3f438ba94
OpesMentis <planchot.antoine@gmail.com>
2018-06-25 21:49:50 +0200

    Réorganisation de la procédure de connexion à la BDD

---
+++ config_dist.php
@@ -0,0 +1,6 @@
+<?php
+$addr = 'your_database_address';
+$db_name = 'your_database_name';
+$db_user  = 'your_database_user';
+$db_pw = 'your_database_password';
+?>
--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 
-if (! file_exists(__DIR__ . '/config.ini')) {
-   echo 'You must copy config.ini.dist to config.ini and fill it with your information';
+if (! file_exists(__DIR__ . '/config.php')) {
+   echo 'You must copy config_dist.php to config.php and fill it with your information';
 }
 
-$ini = parse_ini_file('config.ini');
+include('config.php');
 try {
-    $bdd = new PDO('mysql:host=' . $ini['db_address'] . ';dbname=' . $ini['db_name'] . ';charset=utf8', $ini['db_user'], $ini['db_password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=' . $addr . ';dbname=' . $db_name, $db_user, $db_pw, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
